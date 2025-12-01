b14ecf3a80a8420d9f3bd7acf751db7f87270610
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2025-12-01 10:20:37 +0530

    EditorActivity: force full screen

--- app/src/main/java/io/github/swiftcut/EditorActivity.kt
+++ app/src/main/java/io/github/swiftcut/EditorActivity.kt
@@ -2,16 +2,18 @@ package io.github.swiftcut
 
 import android.os.Bundle
 import android.util.Log
+import android.view.WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import io.github.swiftcut.ui.editor.EditorScreen
 
 class EditorActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        WindowCompat.setDecorFitsSystemWindows(window, false)
-
+        enableFullScreenImmersive()
         setContent {
             SwiftCutTheme {
                EditorScreen()
@@ -21,5 +23,24 @@ class EditorActivity : ComponentActivity() {
 
     override fun onDestroy() {
         super.onDestroy()
+    }
+
+    private fun enableFullScreenImmersive() {
+        with(window) {
+            WindowCompat.setDecorFitsSystemWindows(this, false)
+            val insetsController = WindowInsetsControllerCompat(this, decorView)
+            insetsController.apply {
+                hide(WindowInsetsCompat.Type.systemBars())
+                systemBarsBehavior =
+                    WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            }
+            attributes.layoutInDisplayCutoutMode = LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
+        }
+        applyInsetsToPadOverlay()
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) enableFullScreenImmersive()
     }
 }
