8d7382984013b194d48d82438e48f82d579e5d75
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2025-12-02 23:27:41 +0530

    Create NativeLib.kt

---
+++ app/src/main/java/io/github/swiftcut/NativeLib.kt
@@ -0,0 +1,9 @@
+package io.github.swiftcut
+
+object NativeLib {
+    init {
+        System.loadLibrary("swiftcut_backend")
+    }
+
+    external fun extractThumbnails(videoPath: String, outDir: String, count: Int): Int
+}
