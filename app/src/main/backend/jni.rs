6878ef9bd9d9ca0e623119c765fbcc73af3f9fac
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2025-12-07 18:14:03 +0530

    Update jni.rs

--- app/src/main/backend/jni.rs
+++ app/src/main/backend/jni.rs
@@ -7,26 +7,43 @@ use jni::JNIEnv;
 use std::fs;
 
 #[unsafe(no_mangle)]
-pub extern "C" fn Java_io_github_swiftcut_NativeLib_extractThumbnails(
+pub extern "C" fn Java_io_github_swiftcut_NativeLib_init(
     mut env: JNIEnv,
     _class: JClass,
-    j_video_path: JString,
-    j_out_dir: JString,
+    jRootPath: JString
 ) -> jint {
-    let video_path: String = match env.get_string(&j_video_path) {
-        Ok(s) => s.into(),
-        Err(_) => return -1,
-    };
-    let out_dir: String = match env.get_string(&j_out_dir) {
+    let rootPath: String = match env.get_string(&jRootPath) {
         Ok(s) => s.into(),
         Err(_) => return -2,
     };
 
-    if let Err(_) = fs::create_dir_all(&out_dir) {
+    match core::init(&rootPath) {
+        Ok(_) => 0,
+        Err(_) => -4,
+    }
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn Java_io_github_swiftcut_NativeLib_extractThumbnails(
+    mut env: JNIEnv,
+    _class: JClass,
+    jVideoPath: JString,
+    jOutDir: JString,
+) -> jint {
+    let videoPath: String = match env.get_string(&jVideoPath) {
+        Ok(s) => s.into(),
+        Err(_) => return -1,
+    };
+    let outDir: String = match env.get_string(&jOutDir) {
+        Ok(s) => s.into(),
+        Err(_) => return -2,
+    };
+
+    if let Err(_) = fs::create_dir_all(&outDir) {
         return -3;
     }
 
-    match core::extract_thumbnails(&video_path, &out_dir) {
+    match core::extract_thumbnails(&videoPath, &outDir) {
         Ok(_) => 0,
         Err(_) => -4,
     }
