5592d46ae3c4d05dc01f424fdd1bb8046616cddd
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2025-12-07 22:39:35 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,5 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.compose.compiler) apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.7.10" apply false
-    id("org.mozilla.rust-android-gradle.rust-android") version "0.9.6" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.2.21" apply false
 }
